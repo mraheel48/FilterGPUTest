7d2f7cb859a7dc08a208f4c484c49f4a2c03698e
Muhammad Raheel <57173923+mraheel48@users.noreply.github.com>
2022-01-21 12:56:26 +0500

    filter_done

--- app/src/main/java/com/example/filtergputest/MainActivity.kt
+++ app/src/main/java/com/example/filtergputest/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.constraintlayout.utils.widget.ImageFilterView
 import androidx.core.view.drawToBitmap
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import org.wysaid.nativePort.CGENativeLibrary
 
 
 class MainActivity : AppCompatActivity() {
@@ -52,12 +51,39 @@ class MainActivity : AppCompatActivity() {
             // val ruleString = "@adjust hsl 0.02 -0.31 -0.17"
             // val ruleString = "@curve R(0, 0)(207, 240)(255, 255)"
             //val ruleString = "@curve RGB(0, 0)(177, 166)(255, 255)R(0, 0)(52, 96)(224, 255)(255, 255)"
-           // val ruleString = "@adjust saturation 0.7 @pixblend screen 1 0.243 0.69 1 30"
+            // val ruleString = "@adjust saturation 0.7 @pixblend screen 1 0.243 0.69 1 30"
             //val ruleString = "@adjust saturation 2.0"
-           // val ruleString = "@adjust hue 2.0"
-            val ruleString = MyCustomFilter.EFFECT_CONFIGS[63]
+            //val ruleString = "@adjust saturation 0.6"//saturation is the RGB color Value in the Picture pixel
+            //val ruleString = MyCustomFilter.EFFECT_CONFIGS[103]
 
-            testBitmap = CGENativeLibrary.filterImage_MultipleEffects(testBitmap, ruleString, 1.0f)
+            //val ruleString = "@adjust hsv -1 -1 -1 -1 -1 -1"
+            val ruleString = "#unpack @style emboss 1 2 2 "
+
+            //testBitmap = CGENativeLibrary.filterImage_MultipleEffects(testBitmap, ruleString, 1.0f)
+            // testBitmap = MyCustomFilter.applyHueFilter(testBitmap,3)
+            // testBitmap = MyCustomFilter.applySaturationFilter(testBitmap,3)
+            // testBitmap = MyCustomFilter.applyBlackFilter(testBitmap)
+            //testBitmap = MyCustomFilter.ImageProcessor.tintImage(testBitmap,50)
+            //testBitmap = MyCustomFilter.ImageProcessor2(testBitmap).image
+            //testBitmap = MyCustomFilter.flip(testBitmap,2)
+            //testBitmap = MyCustomFilter.roundCorner(testBitmap,50f)
+            //testBitmap = MyCustomFilter.boost(testBitmap,3,40f)
+            //testBitmap = MyCustomFilter.engrave(testBitmap)
+            //testBitmap = MyCustomFilter.emboss(testBitmap)
+            //testBitmap = MyCustomFilter.smooth(testBitmap,10.0)
+            //testBitmap = MyCustomFilter.applyMeanRemoval(testBitmap)
+            //testBitmap = MyCustomFilter.sharpen(testBitmap,2.0)
+            //testBitmap = MyCustomFilter.applyGaussianBlur(testBitmap)
+            //testBitmap = MyCustomFilter.doBrightness(testBitmap,50)
+            //testBitmap = MyCustomFilter.rotate(testBitmap,270f)
+            //testBitmap = MyCustomFilter.createContrast(testBitmap,50.0)
+            //testBitmap = MyCustomFilter.decreaseColorDepth(testBitmap,32)
+            //testBitmap = MyCustomFilter.createSepiaToningEffect(testBitmap,128,200.0,200.0,0.0)
+            //testBitmap = MyCustomFilter.doColorFilter(testBitmap,50.0,50.0,50.0)
+            //testBitmap = MyCustomFilter.doGamma(testBitmap,1.8,1.8,1.8)
+            //testBitmap = MyCustomFilter.doGreyscale(testBitmap)
+            //testBitmap = MyCustomFilter.doInvert(testBitmap)
+            testBitmap = MyCustomFilter.doHighlightImage(testBitmap)
 
         }, 500)
 
@@ -82,13 +108,13 @@ class MainActivity : AppCompatActivity() {
          Teal	#008080	(0,128,128)	(180°,100%,50%)
          Navy	#000080	(0,0,128)	(240°,100%,50%)*/
 
-        val hsv = floatArrayOf(76f, 1.0f, 0.93f)
+        val hsv = floatArrayOf(160f, 1f, 0.62f)
         rgb_to_hsv(255.0, 38.0, 68.0)
 
         applyFilter.setOnClickListener {
             //applyAdjustment()
 
-            //filterImage.setColorFilter(Color.HSVToColor(hsv), PorterDuff.Mode.MULTIPLY)
+            //filterImage.setColorFilter(Color.HSVToColor(hsv), PorterDuff.Mode.OVERLAY)
 
             filterImage.setImageBitmap(testBitmap)
         }
